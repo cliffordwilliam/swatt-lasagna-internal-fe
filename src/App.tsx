be1b7c4671dd4191f2529df0570ce9a318b97453
Clifford William <ccliffordwilliam@gmail.com>
2026-01-31 15:57:58 +0700

    feat: add clerk

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/clerk-react";
 import HomeIcon from "@mui/icons-material/Home";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { BottomNavigation, BottomNavigationAction, Box } from "@mui/material";
@@ -9,29 +10,46 @@ function App() {
 	const [currentPage, setCurrentPage] = useState(0);
 
 	return (
-		<Box sx={{ pb: 7 }}>
-			{/* Page content */}
-			{currentPage === 0 && <HomePage />}
-			{currentPage === 1 && <OrdersPage />}
-
-			{/* Bottom Navigation */}
-			<BottomNavigation
-				value={currentPage}
-				onChange={(_, newValue) => setCurrentPage(newValue)}
-				showLabels
-				sx={{
-					position: "fixed",
-					bottom: 0,
-					left: 0,
-					right: 0,
-					borderTop: 1,
-					borderColor: "divider",
-				}}
-			>
-				<BottomNavigationAction label="Home" icon={<HomeIcon />} />
-				<BottomNavigationAction label="Orders" icon={<ShoppingCartIcon />} />
-			</BottomNavigation>
-		</Box>
+		<header>
+			<SignedOut>
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						minHeight: "100vh",
+					}}
+				>
+					<SignIn />
+				</Box>
+			</SignedOut>
+			<SignedIn>
+				<Box sx={{ pb: 7 }}>
+					{currentPage === 0 && <HomePage />}
+					{currentPage === 1 && <OrdersPage />}
+					<BottomNavigation
+						value={currentPage}
+						onChange={(_, newValue) => setCurrentPage(newValue)}
+						showLabels
+						sx={{
+							position: "fixed",
+							bottom: 0,
+							left: 0,
+							right: 0,
+							borderTop: 1,
+							borderColor: "divider",
+						}}
+					>
+						<BottomNavigationAction label="Home" icon={<HomeIcon />} />
+						<BottomNavigationAction
+							label="Orders"
+							icon={<ShoppingCartIcon />}
+						/>
+						<UserButton />
+					</BottomNavigation>
+				</Box>
+			</SignedIn>
+		</header>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,13 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<CssBaseline />
-		<App />
+		<ClerkProvider publishableKey="pk_test_ZmFpdGhmdWwtZ2FubmV0LTE2LmNsZXJrLmFjY291bnRzLmRldiQ">
+			<CssBaseline />
+			<App />
+		</ClerkProvider>
 	</StrictMode>,
 );
