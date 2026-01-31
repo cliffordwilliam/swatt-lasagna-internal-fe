39d2f41101c7fd708ed1c059b10d1352158f9ec3
Clifford William <ccliffordwilliam@gmail.com>
2026-01-31 12:37:58 +0700

    feat: add mui

--- src/pages/OrdersPage.tsx
+++ src/pages/OrdersPage.tsx
@@ -1,7 +1,12 @@
+import Button from "@mui/material/Button";
 import type React from "react";
 
 const OrdersPage: React.FC = () => {
-	return <></>;
+	return (
+		<Button variant="contained" color="primary">
+			Hello World
+		</Button>
+	);
 };
 
 export default OrdersPage;
